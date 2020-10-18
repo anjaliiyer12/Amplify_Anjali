cc6dead1c030f2cb622eddb742787613135523c8
anjaliiyer12 <anjali.iyer@csu.fullerton.edu>
2020-10-18 17:33:19 -0400

    Added Auth

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ Amplify.configure(awsExports);
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <signOut>Sign Out</signOut>
+    <signOut></signOut>
   </React.StrictMode>,
   document.getElementById('root')
 );
